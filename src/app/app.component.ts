23fe591b6a92bafc5895601e41bc40b5bb8326bc
Administrator <administrator@mu00294ptommaso.frogdesign.com>
2018-12-05 11:14:51 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as shape from 'd3-shape';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,87 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'NGX-CHART-CUSTOM';
+
+  dataGraph = [
+    {
+      'name': '€',
+      'series': [
+        {
+          'value': 0,
+          'name': new Date('2018-09-1')
+        },
+        {
+          'value': 980,
+          'name': new Date('2018-09-2')
+        },
+        {
+          'value': 980.3,
+          'name': new Date('2018-09-3')
+        },
+        {
+          'value': 980.4,
+          'name': new Date('2018-09-4')
+        },
+        {
+          'value': 980.6,
+          'name': new Date('2018-09-5')
+        }
+        ,
+        {
+          'value': 980.6,
+          'name': new Date('2018-09-6')
+        }
+        ,
+        {
+          'value': 980.6,
+          'name': new Date('2018-09-7')
+        }
+        ,
+        {
+          'value': 980.6,
+          'name': new Date('2018-09-8')
+        }
+        ,
+        {
+          'value': 980.6,
+          'name': new Date('2018-09-9')
+        }
+        ,
+        {
+          'value': 980.6,
+          'name': new Date('2018-09-10')
+        }
+        ,
+        {
+          'value': 980.6,
+          'name': new Date('2018-09-11')
+        }
+        ,
+        {
+          'value': 980.6,
+          'name': new Date('2018-09-12')
+        }
+      ]
+    }
+  ];
+
+  colorScheme = {domain: ['#007AB3']};
+  data: any[] = this.dataGraph;
+  gradient = true;
+  showXAxis = true;
+  showYAxis = true;
+  showLegend = false;
+  showXAxisLabel = false;
+  showYAxisLabel = false;
+  xAxisLabel = 'Time';
+  yAxisLabel = 'Amount';
+  autoScale = true;
+  timeline = false;
+  yScaleMax = 1500; // should be dynamic
+  xScaleMax = new Date('2018-09-30'); // should be dynamic
+  yScaleMin = 0;
+  xScaleMin = new Date('2018-09-1');
+  showGridLines = true;
+  curve = shape.curveLinear;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxChartsModule } from "@swimlane/ngx-charts";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CustomChartComponent } from './custom/custom.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CustomChartComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, NgxChartsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
